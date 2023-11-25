b0c8ecf3560317c0c315f88e428e6b42047f4a7f
Tamal1 <tamalkrishna2026@gmail.com>
2023-11-25 20:37:40 +0600

    Admin Dashboard created

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -22,22 +23,96 @@ async function run() {
   try {
     await client.connect();
     const UserCollection = client.db("BloodDonation").collection("Users");
+    // JWT API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    // MiddleWares
+    const verifyToken = (req, res, next) => {
+      console.log("inside Verify token ", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Forbidden Access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
 
-    app.post("/users", async (req, res) => {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req,res,next)=>{
+      const email = req.decoded.email;
+      const query = {email : email}
+      const user = await UserCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message:'forbidden access'})
+      }
+      next();
+    }
+    // For all users Admin DashBoard
+    app.get("/users", verifyToken, verifyAdmin ,async (req, res) => {
+      console.log(req.headers);
+      const query = req.body;
+      const result = await UserCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "Unauthorized Access" });
+      }
+      const query = { email: email };
+      const user = await UserCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
+    app.post("/users", verifyToken ,verifyAdmin, async (req, res) => {
       try {
         const user = req.body;
-        const query = {email:user.email}
+        const query = { email: user.email };
         const existingUser = await UserCollection.findOne(query);
 
-        if(existingUser){
-          return res.send({message:'user already exists',insertedId:null})
+        if (existingUser) {
+          return res.send({ message: "user already exists", insertedId: null });
         }
         const result = await UserCollection.insertOne(user);
-        res.json({ insertedId: result.insertedId }); // Send the insertedId in the response
+        res.json({ insertedId: result.insertedId }); 
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
       }
+    });
+
+    app.delete("/users/:id",verifyToken,verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await UserCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", verifyToken,verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await UserCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
